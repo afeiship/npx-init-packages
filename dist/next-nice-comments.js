1156a85747799c4a1d4ba7850741b1b80c19db7d
afeiship <1290657123@qq.com>
2019-04-12 16:59:36 +0800

    add: build

---
+++ dist/next-nice-comments.js
@@ -0,0 +1,26 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+  var DEFAULT_TYPES = {
+    js: {
+      start: '/*!\n',
+      end: '\n */'
+    },
+    html: {
+      start: '<!--!\n',
+      end: '\n-->'
+    }
+  };
+
+  nx.niceComments = function(inArray, inType) {
+    var options = DEFAULT_TYPES[inType] || inType;
+    var result = inArray.map(function(item) {
+      return ' * ' + item;
+    });
+    return options.start + result.join('\n') + options.end;
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.niceComments;
+  }
+})();
