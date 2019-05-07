72e5ce70b466c37aecaedec773e461bc5fbbb040
afeiship <1290657123@qq.com>
2019-05-07 23:17:35 +0800

    add: new br

--- src/next-nice-comments.js
+++ src/next-nice-comments.js
@@ -4,11 +4,11 @@
   var DEFAULT_TYPES = {
     js: {
       start: '/*!\n',
-      end: '\n */\n'
+      end: '\n */\n\n'
     },
     html: {
       start: '<!--\n',
-      end: '\n-->\n'
+      end: '\n-->\n\n'
     }
   };
 
