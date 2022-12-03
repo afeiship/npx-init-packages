58504c00a48a506a69071a5038fcf39425ba3438
afeiship <1290657123@qq.com>
2022-12-03 22:38:35 +0800

    fix: rm cjs file

--- packages/craco-init/build/scripts.js
+++ packages/craco-init/build/scripts.js
@@ -4,26 +4,20 @@
   const gulp = require('gulp');
   const tsconfig = require('../tsconfig.json');
   const $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*']
+    pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*'],
   });
-
 
   gulp.task('scripts:cjs', function () {
     return gulp
-      .src('src/**/*.ts')
+      .src('src/index.ts')
       .pipe($.replace('export default ', 'export = '))
       .pipe($.jswork.pkgHeader())
       .pipe($.typescript({ ...tsconfig.compilerOptions, module: 'commonjs' }))
-      .pipe(gulp.dest('dist/cjs'))
+      .pipe(gulp.dest('dist'))
       .pipe($.size({ title: '[ minimize size ]:' }));
   });
 
-  gulp.task('scripts:esm', function () {
-    return gulp
-      .src('src/**/*.ts')
-      .pipe($.jswork.pkgHeader())
-      .pipe($.typescript({ ...tsconfig.compilerOptions, module: 'esnext' }))
-      .pipe(gulp.dest('dist/esm'))
-      .pipe($.size({ title: '[ minimize size ]:' }));
+  gulp.task('scripts:tpls', function () {
+    return gulp.src('src/tpls/*').pipe(gulp.dest('dist/tpls'));
   });
 })();
