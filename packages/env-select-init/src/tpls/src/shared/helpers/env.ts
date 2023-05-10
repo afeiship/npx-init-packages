d3993feeecf2869c222e55a9717cc10fa6076ec4
afeiship <1290657123@qq.com>
2023-05-10 10:08:45 +0800

    fix: env cra default impl

--- packages/env-select-init/src/tpls/src/shared/helpers/env.ts
+++ packages/env-select-init/src/tpls/src/shared/helpers/env.ts
@@ -1,11 +1,11 @@
 import { AbstractEnvSelect } from '@jswork/env-select';
 
-export class Env extends AbstractEnvSelect {
+export default class Env extends AbstractEnvSelect {
   /**
    * Auto select env by current url.
    * @returns {string} The target env string.
    */
   static select(): string {
-    return location.hostname.includes('beta') ? 'beta' : 'production';
+    return window.location.hostname.includes('production') ? 'production' : 'beta';
   }
 }
