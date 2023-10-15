96c20ad6681e9434c343817e1fa1e3ee9a80810a
afeiship <1290657123@qq.com>
2023-10-15 21:56:03 +0800

    feat: add postsas init

---
+++ packages/postsass-init/src/index.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
+import fg from 'fast-glob';
+
+const tpls = path.join(__dirname, 'tpls');
+const files = fg.sync('**/*', { cwd: tpls, dot: true });
+
+execSync('yarn add --dev cssnano autoprefixer');
+
+// copy files:
+files.forEach((file) => {
+  fs.mkdirSync(path.dirname(file), { recursive: true });
+  const src = path.join(tpls, file);
+  const dist = path.join(process.cwd(), file);
+  fs.copyFileSync(src, dist);
+});
+
+console.log('\n@reference: https://js.work/works/b19988493ab54');
+
+process.exit(0);
