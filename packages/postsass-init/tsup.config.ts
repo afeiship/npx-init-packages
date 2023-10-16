1eb116125014e9bf1c885905e861ebc1d2a4503d
afeiship <1290657123@qq.com>
2023-10-16 08:25:53 +0800

    fix: rm global name

--- packages/postsass-init/tsup.config.ts
+++ packages/postsass-init/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/*.ts'],
   format: ['cjs', 'esm'],
-  globalName: 'i18nHelper',
   dts: true,
   sourcemap: true
 });
